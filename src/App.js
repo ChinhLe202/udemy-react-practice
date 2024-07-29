473e43d72ced0cab03e1a491accd8a56daeaaa8b
Le Duy Chinh <chinhld@tandan.com.vn>
2024-07-29 08:42:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world React with Hoi Dan IT
+          React App
         </p>
         <a
           className="App-link"
